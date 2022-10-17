a1a38f144907f8cef834bf3a92bc8ade39fe5744
Vincent Demeester <vdemeest@redhat.com>
2022-10-17 14:59:51 +0200

    packages: fix openshift-install args
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- packages/openshift-install.nix
+++ packages/openshift-install.nix
@@ -65,20 +65,20 @@ rec {
     aarch64-darwin-sha256 = "08nkk77l47ki35d3g692cbkdxcfgfhd10qn6lngvxcdl65fkz848";
     aarch64-linux-sha256 = "1xkw4nrjlq3wpqhpg15m8c5ryfnj22ccmlg1dkzl9fkzdv1y6npf";
     x86_64-darwin-sha256 = "1504ja9rgvvh6aq8fics6nvmklx3m07ygr491vwsky1ics7cjdyi";
-    x86-64-linux-sha256 = "08wpa9ck2bys044xymhz6zr385952mdy96fxbsnqhdcymc1fssfp";
+    x86_64-linux-sha256 = "08wpa9ck2bys044xymhz6zr385952mdy96fxbsnqhdcymc1fssfp";
   };
   openshift-install_4_10 = makeOverridable openshiftInstallGen {
     version = "4.10.37";
     aarch64-darwin-sha256 = "07569yvbahrbda7rpn128pg5ij9rilllc0nlpqy8wz3ssc4gx85i";
     aarch64-linux-sha256 = "1r81d7yh6pkpc0y6ap05r3vr08fk8yrpm70yhzbf7spi47cqmdca";
     x86_64-darwin-sha256 = "0ga9i68qmli6z1hv10rinw2fgxbs3prrh2rbs05yh7gsnk4j56kq";
-    x86-64-linux-sha256 = "0n6y3s85v0kpnnh6d3mvg04inax3qghvfvc1yrlnnn0rsc8v2kdh";
+    x86_64-linux-sha256 = "0n6y3s85v0kpnnh6d3mvg04inax3qghvfvc1yrlnnn0rsc8v2kdh";
   };
   openshift-install_4_9 = makeOverridable openshiftInstallGen {
     version = "4.9.49";
     aarch64-darwin-sha256 = "0i556l9ffirllcdjz7vwb9sg10n6qpa7hahing3n0jrdhppwynjz";
     aarch64-linux-sha256 = "0q6pj92rq0dqbafpv8mi67f3xi4mqs2n075xcfaynvccfr666pm7";
     x86_64-darwin-sha256 = "0y2r9lk9r6gqw82dk4zikkqq8bai009df4yja9n4rp2lq7qndybi";
-    x86-64-linux-sha256 = "031vxspxw5j4y4yjzlgdyb91mra6mz3d6hhf9mfbfcbgsml9sa0x";
+    x86_64-linux-sha256 = "031vxspxw5j4y4yjzlgdyb91mra6mz3d6hhf9mfbfcbgsml9sa0x";
   };
 }

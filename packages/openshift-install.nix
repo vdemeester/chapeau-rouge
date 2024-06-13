306adc4fca5a47aa0bb2c02f477bf8676b61a497
Vincent Demeester <vincent@sbr.pm>
2024-06-13 16:37:09 +0200

    openshift-install: fix target arch "naming"
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- packages/openshift-install.nix
+++ packages/openshift-install.nix
@@ -12,30 +12,30 @@ rec {
 
     let
       # https://mirror.openshift.com/pub/openshift-v4/arm64/clients/ocp/4.9.49/openshift-client-linux.tar.gz
-      getLinuxUrl = version: target:
+      getUrl = version: target:
         if stdenv.isAarch64 then getLinuxAarch64Url version target
         else if stdenv.isx86_64 then getLinuxAmd64Url version target
         else throw "unsupported architecture";
       getLinuxAarch64Url = version: target:
-        if (target == "arm64") then "https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/${version}/openshift-install-linux-${version}.tar.gz"
-        else if (target == "amd64") then "https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/${version}/openshift-install-linux-amd64-${version}.tar.gz"
-        else throw "unsupported target architecture";
+        if (target == "aarch64-linux") then "https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/${version}/openshift-install-linux-${version}.tar.gz"
+        else if (target == "x86_64-linux") then "https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/${version}/openshift-install-linux-amd64-${version}.tar.gz"
+        else throw "unsupported target architecture (${target})";
       getLinuxAmd64Url = version: target:
-        if (target == "arm64") then "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${version}/openshift-install-linux-arm64-${version}.tar.gz"
-        else if (target == "amd64") then "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${version}/openshift-install-linux-${version}.tar.gz"
-        else throw "unsupported target architecture";
+        if (target == "aarch64-linux") then "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${version}/openshift-install-linux-arm64-${version}.tar.gz"
+        else if (target == "x86_64-linux") then "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${version}/openshift-install-linux-${version}.tar.gz"
+        else throw "unsupported target architecture (${target})";
       sha256 = data: target:
         if stdenv.isAarch64 then sha256Aarch64 data target
         else if stdenv.isx86_64 then sha256Amd64 data target
         else throw "unsupported architecture";
       sha256Aarch64 = data: target:
-        if (target == "arm64") then data.linux.aarch64.arm64
-        else if (target == "amd64") then data.linux.aarch64.amd64
-        else throw "unsupported target architecture";
+        if (target == "aarch64-linux") then data.linux.aarch64.arm64
+        else if (target == "x86_64-linux") then data.linux.aarch64.amd64
+        else throw "unsupported target architecture (${target})";
       sha256Amd64 = data: target:
-        if (target == "arm64") then data.linux.amd64.arm64
-        else if (target == "amd64") then data.linux.amd64.amd64
-        else throw "unsupported target architecture";
+        if (target == "aarch64-linux") then data.linux.x86_64.arm64
+        else if (target == "x86_64-linux") then data.linux.x86_64.amd64
+        else throw "unsupported target architecture (${target})";
     in
     stdenv.mkDerivation rec {
       pname = "openshift-install";
@@ -83,11 +83,11 @@ rec {
   };
   openshift-install-arm64_4_15 = makeOverridable openshiftInstallGen {
     versionData = versionsMeta."4.15";
-    target = "arm64";
+    target = "aarch64-linux";
   };
   openshift-install-amd64_4_15 = makeOverridable openshiftInstallGen {
     versionData = versionsMeta."4.15";
-    target = "amd64";
+    target = "x86_64-linux";
   };
   openshift-install_4_14 = makeOverridable openshiftInstallGen {
     versionData = versionsMeta."4.14";
@@ -95,11 +95,11 @@ rec {
   };
   openshift-install-arm64_4_14 = makeOverridable openshiftInstallGen {
     versionData = versionsMeta."4.14";
-    target = "arm64";
+    target = "aarch64-linux";
   };
   openshift-install-amd64_4_14 = makeOverridable openshiftInstallGen {
     versionData = versionsMeta."4.14";
-    target = "amd64";
+    target = "x86_64-linux";
   };
   openshift-install_4_13 = makeOverridable openshiftInstallGen {
     versionData = versionsMeta."4.13";
@@ -107,10 +107,10 @@ rec {
   };
   openshift-install-arm64_4_13 = makeOverridable openshiftInstallGen {
     versionData = versionsMeta."4.13";
-    target = "arm64";
+    target = "aarch64-linux";
   };
   openshift-install-amd64_4_13 = makeOverridable openshiftInstallGen {
     versionData = versionsMeta."4.13";
-    target = "amd64";
+    target = "x86_64-linux";
   };
 }

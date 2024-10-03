1e994980e3cbf24cf1a227a7460c1ecef19a139e
Vincent Demeester <vincent@sbr.pm>
2024-10-03 21:53:36 +0200

    Add opm 1.43 -> 1.47
    Closes #135
    Closes #141
    Closes #161
    Closes #164
    Closes #173
    Closes #178
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- packages/opm.nix
+++ packages/opm.nix
@@ -51,6 +51,31 @@ rec {
       };
     };
 
+  opm_1_47 = makeOverridable opmGen {
+    version = "1.47.0";
+    sha256 = "sha256-EEOlNW6WCuCGp2r4aRzI8AYWwR1mCurCS2PZ9TisN8I=";
+    vendorHash = "sha256-y9ox3DJnuVeAsUOO1PiF9ptVblUt+MJLkvg2eB5W60w=";
+  };
+  opm_1_46 = makeOverridable opmGen {
+    version = "1.46.0";
+    sha256 = "sha256-Kf3QnRy4UvnLgA5p+pIPnGBY53BSFCS7ba1spUMekq4=";
+    vendorHash = "sha256-gQj0v6UVYwJfwELIrKP+xjQb5mDCxUjA/1qAPpsfQOg=";
+  };
+  opm_1_45 = makeOverridable opmGen {
+    version = "1.45.0";
+    sha256 = "sha256-fYjhg/ArgcfsZeKjO5VBgovs0kUoJzf0BgiW3QP62Uk=";
+    vendorHash = "sha256-0SOjxxJWlI38QNupWN6zIZF0ruqavmSV9e/k1UmbQvc=";
+  };
+  opm_1_44 = makeOverridable opmGen {
+    version = "1.44.0";
+    sha256 = "sha256-LneJaLm30h1LDnx6BSqCvAvaIL375oLRRJhJ3JgBdFc=";
+    vendorHash = "sha256-IzDPBu/EnMmJWe5TB4ww8dMhWB6O80OX7XXrAqkz6Vk=";
+  };
+  opm_1_43 = makeOverridable opmGen {
+    version = "1.43.1";
+    sha256 = "sha256-m2TfAAP078GPbV4jXtgHq8OKtg7Yw4GGLYgrPMExZQw=";
+    vendorHash = "sha256-bPbSbcS2a9PHjAxs03CWGOT83/jwKmkKP3Khon+zrMg=";
+  };
   opm_1_42 = makeOverridable opmGen {
     version = "1.42.0";
     sha256 = "sha256-qzEEDmT0xHxRyTDZwD+BwevdYkfNZqeP8gQu3ML6i/M=";
@@ -71,5 +96,5 @@ rec {
     sha256 = "sha256-SnR22t9IDiRTB0xo8gTO9YprgMchp5+T8bGc0GdNfU4=";
     vendorHash = "sha256-Of0ngmLRKXBcWBXgY1hzlQXj10ZcIV3BkbErTk3K3zw=";
   };
-  opm = opm_1_42;
+  opm = opm_1_47;
 }

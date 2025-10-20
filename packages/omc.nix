175ea55ab1d0d60071a4149a2bc25ac2e7646aed
Vincent Demeester <vincent@sbr.pm>
2025-10-20 10:09:22 +0200

    omc: add 3.10, 3.11 and 3.12..
    .. and clean the old ones
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- packages/omc.nix
+++ packages/omc.nix
@@ -1,4 +1,9 @@
-{ stdenv, lib, buildGoModule, fetchFromGitHub }:
+{ stdenv
+, lib
+, buildGoModule
+, fetchFromGitHub
+,
+}:
 
 with lib;
 rec {
@@ -6,6 +11,7 @@ rec {
     { version
     , sha256
     , rev ? "v${version}"
+    ,
     }:
     buildGoModule rec {
       pname = "omc";
@@ -54,27 +60,19 @@ rec {
       };
     };
 
-  omc_3_7 = makeOverridable omcGen {
-    version = "3.7.4";
-    sha256 = "sha256-0omvZY3Sqr/wcKKUyfWIfr7vQY534N/EmpPLaS4TUIE=";
+  omc_3_12 = makeOverridable omcGen {
+    version = "3.12.0";
+    sha256 = "sha256-C737p7yCSkdKxBiw3fq0a9ZZRr6myLAx20cJnE7HZTU=";
   };
-  omc_3_6 = makeOverridable omcGen {
-    version = "3.6.3";
-    sha256 = "sha256-33VjDhGG91CH4vaO9v0KnI+bAZPg7MmDClYhxNOPxfQ=";
+  omc_3_11 = makeOverridable omcGen {
+    version = "3.11.2";
+    sha256 = "sha256-TTYigS2epmJ37SBBZQGTKyR40r2txhvzNM1RMM8jkcY=";
   };
-  omc_3_4 = makeOverridable omcGen {
-    version = "3.4.0";
-    sha256 = "sha256-4SCb4Ao31RRRlqr2kxBNPxsn00XewV2Lrf1RyGvM1N0=";
+  omc_3_10 = makeOverridable omcGen {
+    version = "3.10.0";
+    sha256 = "sha256-BpR/Ts/IJGnzoGE3jzv6LeE322L62Xpv9ojP6MVMjIk=";
   };
-  omc_3_3 = makeOverridable omcGen {
-    version = "3.3.2";
-    sha256 = "sha256-Fjum6OU166aRckj75KIDEgwyTFX8/P528pQquYoqWGI=";
-  };
-  omc_3_2 = makeOverridable omcGen {
-    version = "3.2.0";
-    sha256 = "sha256-GBfRjS8u+KKBLie2GmzeLbD08V/LKNF7cbC7fjMxUtw=";
-  };
-  omc = omc_3_7;
+  omc = omc_3_12;
 
   omc-git =
     let
